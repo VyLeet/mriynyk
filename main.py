550383dfe65fc34fb78fe967394da59c85cc5d5f
Nazarii Vysokinskyi <n.vysokinskyi@gmail.com>
2026-01-13 17:30:32 +0200

    feature(CLI): Add argument parsing

--- main.py
+++ main.py
@@ -1,10 +1,42 @@
+from argparse import ArgumentParser, ArgumentTypeError, Namespace
+from enum import Enum, StrEnum, auto
+from typing import Final, Sequence
+
 import pandas as pd
 
 
-def main():
-    df = pd.read_parquet("/Users/admin/Developer/.personal/mriynyk/data/Lapathon2026 Mriia public files/benchmark_scores.parquet")
-    print(df.head(100))
+class Year(Enum):
+    eight = 8
+    nine = 9
+
+    def __str__(self) -> str:
+        return str(self.value)
+
+class Subject(StrEnum):
+    ukrainian_language = auto()
+    ukrainian_history = auto()
+    algebra = auto()
+
+def parse_args() -> Namespace:
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--year",
+        required=True,
+        type=lambda year: Year(int(year)),
+        choices=tuple(Year),
+    )
+    parser.add_argument(
+        "--subject",
+        required=True,
+        type=Subject,
+        choices=tuple(Subject),
+    )
+    return parser.parse_args()
+
+
+def main() -> None:
+    pass
 
 
 if __name__ == "__main__":
-    main()
+    args = parse_args()
